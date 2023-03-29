e8b288424f6d8ce819b1472fe0dfa44362b2df64
Pfsenses <alex@pecherskih.name>
2023-03-29 12:18:05 +0300

    Initial commit

---
+++ Get-Free-Space.ps1
@@ -0,0 +1,6 @@
+#Get Free Space for All hosts in selected OU
+Import-Module ActiveDirectory
+$Winservers=(Get-ADComputer -Filter *  -searchbase "OU=Servers,DC=EXAMPLE,DC=ORG" -SearchScope 2).Name
+$Total=foreach ($item in $Winservers){Invoke-Command -ComputerName $item {Get-PSDrive -PSProvider FileSystem} | Select-Object PSComputerName,Name,@{Name="UsedSpace";Expression={$_.Used/1GB}}}
+if (!(Test-Path -Path C:\Scripts\CSV)){New-Item -ItemType Directory -Path C:\Scripts\CSV | Out-Null}
+$Total | Where-Object UsedSpace -ne "0" | Export-CSV C:\Scripts\CSV\FreeSpace.csv
