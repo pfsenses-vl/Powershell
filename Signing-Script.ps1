7211c1d8a4e24143c2ccee491639e9c9530ec5f3
pfsenses <alex@pecherskih.name>
2023-03-30 12:06:42 +0300

    Comment Update

--- Signing-Script.ps1
+++ Signing-Script.ps1
@@ -1,4 +1,6 @@
+###################################################################################################################################
 #This script require Deployed CA Service,Alowed CA Template CodeSigning, and installed certificate, which will be used for signing:
+###################################################################################################################################
 $LocalCert=(Get-ChildItem -Path Cert:\CurrentUser\My).EnhancedKeyUsageList.FriendlyName 
 if (!($LocalCert)) {Set-Location -Path cert:\CurrentUser\My | Out-Null;Get-Certificate -Template CodeSigning | Out-Null;Start-Sleep -Seconds 2}
 $cert = (Get-ChildItem cert:\CurrentUser\my –CodeSigningCert)
