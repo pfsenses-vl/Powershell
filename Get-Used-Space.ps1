05c83fcac8bb64eb21c2394b90dfc5c42896c614
pfsenses <alex@pecherskih.name>
2023-03-30 12:02:31 +0300

    Comment Update

--- Get-Used-Space.ps1
+++ Get-Used-Space.ps1
@@ -1,4 +1,6 @@
+############################################
 #Get Used Space for All hosts in selected OU
+############################################
 Import-Module ActiveDirectory
 $Winservers=(Get-ADComputer -Filter *  -searchbase "OU=Servers,DC=EXAMPLE,DC=ORG" -SearchScope 2).Name
 $Total=foreach ($item in $Winservers){Invoke-Command -ComputerName $item {Get-PSDrive -PSProvider FileSystem} | Select-Object PSComputerName,Name,@{Name="UsedSpace";Expression={$_.Used/1GB}}}
